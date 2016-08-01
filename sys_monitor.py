22d93d77bbd184e74f06107ae288c1103aee0cb7
yevgeny hong <yev_science@naver.com>
2016-08-01 09:38:37 +0900

    Update sys_monitor.py

--- sys_monitor.py
+++ sys_monitor.py
@@ -17,6 +17,7 @@ while (True):
     cpu_p = int(ps.cpu_percent())
     mem_p = int(ps.virtual_memory().percent)
     disk_p = int(ps.disk_usage('/').percent)
+    #HEX 데이터로 보내기 시,분,CPU,RAM,D 각각 2바이트씩 총 10바이트다.
     ser.write(str.encode("h"))
     ser.write(hour)
     ser.write(str.encode("m"))
@@ -27,4 +28,4 @@ while (True):
     ser.write(bytes([mem_p]))
     ser.write(str.encode("D"))
     ser.write(bytes([disk_p]))
-    time.sleep(2)
+    time.sleep(2)#2초간 대기, LCD업데이트속도와, 아두이노 통신속도를 고려했을때, 이게 가장 적합한듯하다.
